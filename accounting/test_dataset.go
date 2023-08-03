62b8bc857c50cfb0b29fedf7a5cfb80bf175264e
Ben Baldivia <ben.baldivia@tridge.com>
2023-08-03 12:24:21 +0900

    wip

--- accounting/test_dataset.go
+++ accounting/test_dataset.go
@@ -3,6 +3,7 @@ package accounting
 import (
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	gofakeit "github.com/brianvoe/gofakeit/v6"
@@ -16,71 +17,86 @@ type TestDataset struct {
 	Categories   []Category
 }
 
+var payees = []string{}
+var payeeOnce = sync.Once{}
+
 func GetTestDatasetPayee() []string {
-	faker := gofakeit.New(901)
-	p := make([]string, 20)
-	for i, _ := range p {
-		p[i] = faker.Name()
-	}
-	return p
+	payeeOnce.Do(func() {
+		faker := gofakeit.New(901)
+		for i := 0; i < 20; i++ {
+			payees = append(payees, faker.Name())
+		}
+	})
+	return payees
 }
+
+var accounts = []Account{}
+var accountOnce = sync.Once{}
 
 func GetTestDatasetAccounts() []Account {
-	faker := gofakeit.New(101)
-	a := make([]Account, 10)
-	for i, _ := range a {
-		a[i] = Account{
-			Id:             uuid.MustParse(faker.UUID()),
-			Name:           faker.Name(),
-			CurrencySymbol: currency.CurrencySymbol("USD"),
+	accountOnce.Do(func() {
+		faker := gofakeit.New(101)
+		for i := 0; i < 10; i++ {
+			accounts = append(accounts, Account{
+				Id:             uuid.MustParse(faker.UUID()),
+				Name:           faker.Name(),
+				CurrencySymbol: currency.CurrencySymbol("USD"),
+			})
 		}
-	}
-	return a
+	})
+	return accounts
 }
 
+var cats = []Category{}
+var catOnce = sync.Once{}
+
 func GetTestDatasetCategories() []Category {
-	cats := []Category{}
-	exist := make(map[string]bool)
-	faker := gofakeit.New(102)
-	for i := 0; i <= 10; i++ {
-		name := faker.Noun()
-		i := -1
-		k := strings.Join([]string{name}, "-")
-		for exist[k] {
-			i++
-			k = strings.Join([]string{name, strconv.Itoa(i)}, "-")
+	catOnce.Do(func() {
+		exist := make(map[string]bool)
+		faker := gofakeit.New(102)
+		for i := 0; i <= 10; i++ {
+			name := faker.Noun()
+			i := -1
+			k := strings.Join([]string{name}, "-")
+			for exist[k] {
+				i++
+				k = strings.Join([]string{name, strconv.Itoa(i)}, "-")
+			}
+			exist[k] = true
+			cats = append(cats, Category{
+				Id:    uuid.MustParse(faker.UUID()),
+				Value: k,
+			})
 		}
-		exist[k] = true
-		cats = append(cats, Category{
-			Id:    uuid.MustParse(faker.UUID()),
-			Value: k,
-		})
-	}
+	})
 	return cats
 }
 
+var tx = []Transaction{}
+var txOnce = sync.Once{}
+
 func GetTestDatasetTransactions() []Transaction {
-	accounts := GetTestDatasetAccounts()
-	payees := GetTestDatasetPayee()
-	cats := GetTestDatasetCategories()
-	faker := gofakeit.New(102)
-	t := make([]Transaction, 10000)
-	_ = t
-	for i, _ := range t {
-		t[i] = Transaction{
-			Id:        uuid.MustParse(faker.UUID()),
-			AccountId: accounts[faker.IntRange(0, len(accounts)-1)].Id,
-			At: faker.DateRange(
-				time.Date(2018, 01, 01, 0, 0, 0, 0, time.UTC),
-				time.Date(2023, 07, 19, 23, 59, 0, 0, time.UTC),
-			),
-			Payee:      payees[faker.IntRange(0, len(payees)-1)],
-			Amount:     faker.IntRange(-1000, 1100),
-			Note:       faker.LoremIpsumSentence(4),
-			CategoryId: cats[faker.IntRange(0, len(cats)-1)].Id,
+	txOnce.Do(func() {
+		accounts := GetTestDatasetAccounts()
+		payees := GetTestDatasetPayee()
+		cats := GetTestDatasetCategories()
+		faker := gofakeit.New(102)
+		for i := 0; i < 10000; i++ {
+			tx = append(tx, Transaction{
+				Id:        uuid.MustParse(faker.UUID()),
+				AccountId: accounts[faker.IntRange(0, len(accounts)-1)].Id,
+				At: faker.DateRange(
+					time.Date(2018, 01, 01, 0, 0, 0, 0, time.UTC),
+					time.Date(2023, 07, 19, 23, 59, 0, 0, time.UTC),
+				),
+				Payee:      payees[faker.IntRange(0, len(payees)-1)],
+				Amount:     faker.IntRange(-1000, 1100),
+				Note:       faker.LoremIpsumSentence(4),
+				CategoryId: cats[faker.IntRange(0, len(cats)-1)].Id,
+			})
 		}
-	}
-	return t
+	})
+	return tx
 }
 
 func GetTestDataset() *TestDataset {
